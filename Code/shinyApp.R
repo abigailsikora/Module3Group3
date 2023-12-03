d495fd42b0bccb608ba05850f3079fc2d1e3b441
abigailsikora <143763564+abigailsikora@users.noreply.github.com>
2023-12-03 17:03:00 -0600

    Update shinyApp.R

--- Code/shinyApp.R
+++ Code/shinyApp.R
@@ -4,6 +4,11 @@ library(leaflet)
 library(ggplot2)
 library(dplyr)
 library(tidytext)
+library(tidyr)
+
+
+sentiment_scores_by_postal_code <- read.csv("sentiment_by_postal_code.csv")
+sentiment_scores_business_id <- read.csv("sentiment_scores_by_business.csv")
 # Read the JSON files
 business_data <- fromJSON('nashville_bars_nightlife.json')
 census <- read.csv("census_postalcodes.csv")
@@ -18,7 +23,10 @@ census <- census %>%
     `Businesses` = business
   )
 
-
+drinksdf <- data.frame( 
+    Drink=c("Cocktail", "Wine", "Beer", "Tequila", "Whisky", "Brandy", "Vodka", "Sake"),
+    `Sentiment Score`=c("0.30", "0.29", "0.27" ,"0.27","0.27","0.27","0.22","0.22")
+  )
 
 # Create a data frame from the first JSON file
 business <- as.data.frame(business_data)
@@ -69,18 +77,45 @@ ui <- fluidPage(
              )
     ),
     tabPanel("Choose a Business", 
-             selectInput("postalCode", "Select Postal Code", choices = unique_postal_codes),
-             selectInput("business", "Select a Business", choices = unique_business_names),
-             tableOutput("censusPlot"),
-             plotOutput("bus")
+             fluidRow(
+               column(3,
+                      selectInput("postalCode", "Select Postal Code", choices = unique_postal_codes),
+                      selectInput("business", "Select a Business", choices = unique_business_names)
+               ),
+               column(9,
+                      h4("U.S. Census Data"),
+                      tableOutput("censusPlot"),
+                      h4("Number of Reviews Over Time"),
+                      plotOutput("bus"),
+                      plotOutput("sentimentBarPlot")
+               )
+             )
     ),
     tabPanel("Advice", 
-             HTML("<div class='advice-container'>
-                    <h4>Advice to consider:</h4>
-                    <p>1. Happy hours do not impact star rating by much. The average star difference between those that do have a happy hour and those that don't is less than 0.3. Therefore it is not important in considering having a happy hour or not.</p>
-                  </div>"),
-             plotOutput("happyhour"))
-  )
+             fluidRow(
+               column(6, 
+                      HTML("<div class='advice-container'>
+              <h4>Advice to consider:</h4>
+              <p>1. Happy hours do not impact star rating as discovered in two-sample t-tests. The average star difference between those that do have a happy hour and those that don't is less than 0.3. Therefore it is not important in considering having a happy hour or not.</p>
+            </div>"),
+                      HTML("<div class='advice-container'>
+              <p>2. Location is important as discovered in ANOVA testing. Specifically, we advise opening a business in 37216 over 37221.</p>
+            </div>"),
+                      HTML("<div class='advice-container'>
+              <p>3. Cocktails are the best drink to serve.It has the highest sentiment score. </p>
+            </div>")
+               ),
+               HTML("<div class='advice-container'>
+              <p>4. We recommend you improve wait times as customers are sensitive to wait. 22.6% of reviews mentioned wait.</p>
+            </div>")
+             ),
+               column(6, 
+                      plotOutput("happyhour"),
+                      plotOutput("postalcodeplot"),
+                      tableOutput("drinks")
+               )
+             )
+    )
 )
 
 
@@ -127,7 +162,8 @@ server <- function(input, output, session) {
       geom_bar(stat = "summary", fun = "mean", position = "dodge", fill = "skyblue") +
       labs(title = "Impact of Happy Hour on Star Rating",
            x = "Happy Hour",
-           y = "Average Star Rating")
+           y = "Average Star Rating")+
+      theme_minimal()
   })
   output$averageInfo <- renderPrint({
     avg_stars <- mean(business$stars, na.rm = TRUE)
@@ -145,19 +181,76 @@ server <- function(input, output, session) {
       filter(name == input$business)
     reviews_filtered <- reviews %>%
       filter(business_id == business_filtered$business_id) %>%
-      mutate(date =as.Date(date),
+      mutate(date = as.Date(date),
              year = lubridate::year(date))
-    ggplot(reviews_filtered, aes(x = date, y = stars)) +
-      geom_point() +
-      labs(title = paste("Number of Reviews Over Time for", input$business),
-           x = "Date",
-           y = "Star Rating") +
+    
+    # Calculate counts for each combination of year and star rating
+    heatmap_data <- reviews_filtered %>%
+      group_by(year, stars) %>%
+      summarize(count = n())
+    
+    ggplot(heatmap_data, aes(x = year, y = stars, fill = count)) +
+      geom_tile() +
+      scale_fill_viridis_c() +
+      labs(title = paste("Heatmap of Reviews Over Time for", input$business),
+           x = "Year",
+           y = "Star Rating",
+           fill = "Number of Reviews") +
       theme_minimal()
-})
-
-
+  })
   
+  
+  
+  output$postalcodeplot <- renderPlot({
+    business %>%
+      group_by(postal_code) %>%
+      summarise(mean_stars = mean(stars, na.rm = TRUE)) %>%
+      arrange(desc(mean_stars)) %>%
+      ggplot(aes(x = mean_stars, y = reorder(postal_code, mean_stars))) +
+      geom_bar(stat = "identity", fill = "skyblue") +
+      labs(title = "Average Star Rating by Postal Code",
+           x = "Average Star Rating",
+           y = "Postal Code") +
+      theme_minimal()
+  })
+  output$drinks <-renderTable({
+    drinksdf
+  })
+  business_details <- reactive({
+    selected_business_info <- sentiment_scores_business_id[sentiment_scores_business_id$business_id == unique_business_ids[business$name == input$business], ]
+    selected_business_info
+  })
+  output$sentimentBarPlot <- renderPlot({
+    # Get the sentiment analysis results using the reactive function
+    #sentiment_data <- sentiment_analysis()
+    
+    # Reshape the data for plotting (if needed, depending on your data format)
+    business_scores <- pivot_longer(business_details(), col=colnames(business_details())[-c(1,2)], names_to='variable', values_to='value')
+    
+    # Filter sentiment scores by the selected postal code
+    selected_postal_code <- input$postalCode
+    postal_code_score_avgs <- subset(sentiment_scores_by_postal_code, postal_code == selected_postal_code)
+    postal_code_score_avgs <- pivot_longer(postal_code_score_avgs, col=colnames(postal_code_score_avgs)[-1], names_to='variable', values_to='value')
+    
+    
+    # Plotting the sentiment analysis data as a bar plot
+    #TODO: add avg black bars here. another layer of geom_bar()
+    ggplot(NULL, aes(variable, value)) +
+      geom_bar(aes(fill="tomato3"), data=business_scores, stat = 'identity', show.legend = F) +
+      geom_bar(aes(fill='steelblue3'), data=postal_code_score_avgs, stat = 'identity', show.legend = F) +
+      #geom_bar(data = business_scores, aes(x = business_id, y = value, fill = variable), stat = "identity", position = "dodge") +
+      #geom_bar(data = postal_code_scores, aes(x = postal_code, y = value), fill = "black", alpha = 0.5, stat ="identity", position = "dodge") +
+      labs(title = "Sentiment Analysis by Aspect Across Businesses",
+           y = "Sentiment Score") +
+      theme_classic()
+    #theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    #scale_fill_brewer(palette = "Set3") +  # Change the color palette if needed
+    
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+#some of the the above code is from ChatGPT 3.5. Accessed the week of 11/27.
